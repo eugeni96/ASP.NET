248f6b7a8183b8a15f8ca685e775c38d7ecbec72
Eugeni96 <e.a.tseitlin@gmail.com>
2014-09-23 22:24:46 +0300

    lesson 3 added

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LessonProject.Model;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new LessonProjectDbDataContext(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+            var roles = context.Roles.ToList();
+            foreach (var role in roles)
+            {
+                Console.WriteLine("{0} {1} {2}", role.ID, role.Code, role.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
