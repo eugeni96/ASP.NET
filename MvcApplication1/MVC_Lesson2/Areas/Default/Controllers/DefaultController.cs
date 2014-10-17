e862c78faa58290cef0821371bff540552fcfbd2
Eugeni96 <e.a.tseitlin@gmail.com>
2014-10-17 17:03:41 +0300

    add controllers

---
+++ MvcApplication1/MVC_Lesson2/Areas/Default/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Lesson2.Controllers;
+
+namespace MVC_Lesson2.Areas.Default.Controllers
+{
+    public class DefaultController : BaseController
+    {
+        //
+        // GET: /Default/Default/
+
+    }
+}
---
+++ MvcApplication1/MVC_Lesson2/Areas/Default/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Lesson2.Controllers;
+using MVC_Lesson2.Models.ViewModels;
+
+namespace MVC_Lesson2.Areas.Default.Controllers
+{
+    public class LoginController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new LoginView());
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginView loginView)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = Auth.Login(loginView.Email, loginView.Password, loginView.IsPersistent);
+                if (user != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState["Password"].Errors.Add("Пароли не совпадают");
+            }
+            return View(loginView);
+        }
+
+        public ActionResult Logout()
+        {
+            Auth.LogOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
