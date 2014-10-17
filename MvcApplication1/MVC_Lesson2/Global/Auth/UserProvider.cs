8f2823f1c766aa6cdb321363b20ac6ef93ef42dc
Eugeni96 <e.a.tseitlin@gmail.com>
2014-10-17 14:36:23 +0300

    add UserProvider

--- MvcApplication1/MVC_Lesson2/Global/Auth/UserProvider.cs
+++ MvcApplication1/MVC_Lesson2/Global/Auth/UserProvider.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
+using LessonProject.Model;
 
 namespace MVC_Lesson2.Global.Auth
 {
-    public class UserProvider
+    public class UserProvider : IPrincipal
     {
+        private UserIndentity userIdentity { get; set; }
+
+        #region IPrincipal Members
+
+        public IIdentity Identity
+        {
+            get { return userIdentity; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            if (userIdentity.User == null)
+            {
+                return false;
+            }
+            return userIdentity.User.InRoles(role);
+        }
+
+        #endregion
+
+
+        public UserProvider(string name, IRepository repository)
+        {
+            userIdentity = new UserIndentity();
+            userIdentity.Init(name, repository);
+        }
+
+
+        public override string ToString()
+        {
+            return userIdentity.Name;
+        }
+
     }
 }
