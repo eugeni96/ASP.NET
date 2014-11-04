35da75d1b552611d1406b2e7c5a8f7bb0c8d4a34
Eugeni96 <e.a.tseitlin@gmail.com>
2014-11-04 14:09:22 +0300

    add SearchEngine

---
+++ MvcApplication1/MVC_Lesson2/Global/SearchEngine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using LessonProject.Model;
+
+namespace MVC_Lesson2.Global
+{
+    public class SearchEngine
+    {
+        private static readonly Regex RegexStripHtml = new Regex("<[^>]*>", RegexOptions.Compiled);
+
+
+        private static string StripHtml(string html)
+        {
+            return string.IsNullOrWhiteSpace(html) ? string.Empty :
+                RegexStripHtml.Replace(html, string.Empty).Trim();
+        }
+
+        private static string CleanContent(string content, bool removeHtml)
+        {
+            if (removeHtml)
+            {
+                content = StripHtml(content);
+            }
+
+            content =
+                content.Replace("\\", string.Empty).
+                Replace("|", string.Empty).
+                Replace("(", string.Empty).
+                Replace(")", string.Empty).
+                Replace("[", string.Empty).
+                Replace("]", string.Empty).
+                Replace("*", string.Empty).
+                Replace("?", string.Empty).
+                Replace("}", string.Empty).
+                Replace("{", string.Empty).
+                Replace("^", string.Empty).
+                Replace("+", string.Empty);
+
+            var words = content.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            var sb = new StringBuilder();
+            foreach (var word in
+                words.Select(t => t.ToLowerInvariant().Trim()).Where(word => word.Length > 1))
+            {
+                sb.AppendFormat("{0} ", word);
+            }
+
+            return sb.ToString();
+        }
+        public static IEnumerable<User> Search(string searchString, IQueryable<User> source)
+        {
+            var term = CleanContent(searchString.ToLowerInvariant().Trim(), false);
+            var terms = term.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var regex = string.Format(CultureInfo.InvariantCulture, "({0})", string.Join("|", terms));
+
+            foreach (var entry in source)
+            {
+                var rank = 0;
+
+
+                if (!string.IsNullOrWhiteSpace(entry.Email))
+                {
+                    rank += Regex.Matches(entry.Email.ToLowerInvariant(), regex).Count;
+                }
+                if (rank > 0)
+                {
+                    yield return entry;
+                }
+            }
+        }
+    }
+
+}
