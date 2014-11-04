110acc038abd7831a863e4abebf8c367bdb64348
Eugeni96 <e.a.tseitlin@gmail.com>
2014-11-04 14:39:22 +0300

    add Project.Tools with WebExtensions class

---
+++ MvcApplication1/LessonProject.Tools/WebExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace LessonProject.Tools
+{
+    static class WebExtensions
+    {
+        public static MvcHtmlString NlToBr(this string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return new MvcHtmlString(string.Empty);
+            }
+            return new MvcHtmlString(source.Replace(Environment.NewLine, "<br />"));
+        }
+
+        public static string Teaser(this string content, int length, string more = "...")
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return string.Empty;
+            }
+
+            if (content.Length < length)
+            {
+                return content;
+            }
+
+            return content.Substring(0, length) + more;
+        }
+
+        public static string CountWord(this int count, string first, string second, string five)
+        {
+            if (count % 10 == 1 && (int)(count / 10) != 1)
+            {
+                return first;
+            }
+            if (count % 10 > 1 && count % 10 < 5 && ((int)(count / 10) % 10) != 1)
+            {
+                return second;
+            }
+            return five;
+        }
+    }
+
+
+}
