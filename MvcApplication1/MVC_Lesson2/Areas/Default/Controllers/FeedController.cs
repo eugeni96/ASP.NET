10746d00ce479662aa119206c883db8fb8dd2f85
Eugeni96 <e.a.tseitlin@gmail.com>
2014-11-06 23:45:35 +0300

    add rss

---
+++ MvcApplication1/MVC_Lesson2/Areas/Default/Controllers/FeedController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using MVC_Lesson2.Global;
+
+namespace MVC_Lesson2.Areas.Default.Controllers
+{
+    public class FeedController : DefaultController
+    {
+        //
+        // GET: /Default/Feed/
+
+        public ActionResult Index()
+        {
+            var host = Request.Url;
+            var feed =
+               new SyndicationFeed("Site RSS",
+                                   "",
+                                   new Uri(host.AbsoluteUri + "/Feed"));
+
+            var items = new List<SyndicationItem>();
+
+            var item = new SyndicationItem(
+               "Title",
+               "content",
+               new Uri("http://" + host + "/some-link-url"),
+               "Title",
+               DateTime.Now
+               );
+            items.Add(item);
+            feed.Items = items;
+
+            return new RssActionResult { Feed = feed };
+        }
+
+
+    }
+}
